f017874baa31618bfdf02109487fe918cc909143
tida17602 <matidapelemba@gmail.com>
2024-03-25 09:40:29 +0200

    Number manipulation and F strings

---
+++ Day2-BMI_Calculator.py
@@ -0,0 +1,11 @@
+height = input("Enter your height: ")
+weight = input("Enter your weight: ")
+
+weight_as_int = int(weight)
+height_as_float = float(height)
+
+bmi = weight_as_int / height_as_float ** 2
+
+bmi_as_int = int(bmi)
+
+print(bmi_as_int)
